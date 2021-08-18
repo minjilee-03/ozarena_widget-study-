4086430a7151c01671e41adfacede32ddaf64d11
minjilee-03 <56942604+minjilee-03@users.noreply.github.com>
2021-08-18 11:30:39 +0900

    더보기 버튼 토글 추가

--- js/index.js
+++ js/index.js
@@ -9,14 +9,24 @@ $(document).ready(function () {
     $('.battlica-event-box-wrap').show();
     $('.academy-event-box-wrap').hide();
     $('.battlica-title').click(function () {
-        $('.academy-event-box-wrap').hide(); 
-        $('.battlica-event-box-wrap').show(); 
+        $('.academy-event-box-wrap').hide();
+        $('.battlica-event-box-wrap').show();
+        $('.academy-more-event-box-wrap').hide();
         return false;
     });
     $('.academy-title').click(function () {
-        $('.battlica-event-box-wrap').hide(); 
-        $('.academy-event-box-wrap').show(); 
+        $('.battlica-event-box-wrap').hide();
+        $('.academy-event-box-wrap').show();
+        $('.battlica-more-event-box-wrap').hide();
         return false;
     });
 });
+
+$(document).ready(function () {
+    $('.more-event-box-wrap').hide();
+    $('.more-container-wrap').click(function () {
+        $('.academy-more-event-box-wrap').slideToggle(1200);
+    });
+});
+
 
